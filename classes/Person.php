ad28f3f05aebccdc529ba97041d291dc1dd00b73
rc2pc2 <petriccarcc@gmail.com>
2023-07-14 12:34:30 +0200

    added up both explicit properties and methods

--- classes/Person.php
+++ classes/Person.php
@@ -2,21 +2,43 @@
 include_once __DIR__ . '/LivingBeing.php';
 
 class Person extends LivingBeing {
-    public $name;
-    public $address;
+    protected $name;
+    protected $address;
+    protected $isAdult;
     public static $staticProperty = 'VERSION 1.0';
 
     function __construct(String $_name, DateTime $_dateOfBirth, Address $_address){
         parent::__construct($_dateOfBirth);
-        $this->name = $_name;
-        $this->address = $_address;
+        $this->setName($_name);
+        $this->setAddress($_address);
+        $this->setAdult();
+    }
+
+    private function setName(String $name){
+        $this->name = $name;
+    }
+
+    private function setAddress(Address $address){
+        $this->address = $address;
+    }
+
+    private function setAdult(){
+        if ( $this->getAge() >= 18){
+            $this->isAdult = true;
+        } else {
+            $this->isAdult = false;
+        }
     }
 
     public function getName(){
         return $this->name;
     }
 
-    public function eat(){
+    public function getAddress(){
+        return $this->address;
+    }
+
+    protected function eat(){
         return parent::eat() . ' crunch, crunch, crunch...';
     }
 }
